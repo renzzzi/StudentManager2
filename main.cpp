5da7a7a9ae40008619d714b9d8e9ea5edbb30219
renzzzi <renzdavidespiritu2006@gmail.com>
2025-04-23 23:43:46 +0800

    Added a display menu.

--- main.cpp
+++ main.cpp
@@ -11,3 +11,18 @@ struct Student {
     Student(std::string name, std::string studentId, std::string course, int year, float grade) 
     : name(name), studentId(studentId), course(course), yearLevel(year), finalGrade(grade) {}
 };
+
+void displayMenu()
+{
+    std::cout << "\n1. Add a student record\n";
+    std::cout << "2.View all records\n";
+    std::cout << "3. Search a record by Student ID\n";
+    std::cout << "4. Update a record\n";
+    std::cout << "5. Exit\n";
+}
+
+int main()
+{
+
+    return 0;
+}
